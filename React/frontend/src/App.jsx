e7214bbf1b8a56dc0e5df06f94d05478c8c0b733
getanech <127737390+getanech@users.noreply.github.com>
2024-09-24 11:34:56 +0300

    exept

--- React/frontend/src/App.jsx
+++ React/frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
-
 import { UserProvider } from "./context/UserContext";
 import Main from "./pages/MainManu";
 import Workout from "./pages/Workout/Workout";
